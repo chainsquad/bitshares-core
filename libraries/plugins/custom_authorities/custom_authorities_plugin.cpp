36651f033510a3f7e513eb9bdeea6083c99cfa2c
Roman Mokych <roma@apriorit.com>
2018-12-20 13:03:15 +0200

    Removed redundand code from the custom_auth plugin.

--- libraries/plugins/custom_authorities/custom_authorities_plugin.cpp
+++ libraries/plugins/custom_authorities/custom_authorities_plugin.cpp
@@ -34,49 +34,31 @@ class custom_authorities_plugin_impl
    public:
       custom_authorities_plugin_impl(custom_authorities_plugin& _plugin)
          : _self( _plugin )
-      {  }
-      virtual ~custom_authorities_plugin_impl();
-
-      void onBlock( const signed_block& b );
+      {}
 
       graphene::chain::database& database()
       {
          return _self.database();
       }
 
+private:
       custom_authorities_plugin& _self;
-
-      std::string _plugin_option = "";
-
-   private:
-
 };
-
-void custom_authorities_plugin_impl::onBlock( const signed_block& b )
-{
-   wdump((b.block_num()));
-}
-
-custom_authorities_plugin_impl::~custom_authorities_plugin_impl()
-{
-   return;
-}
 
 } // end namespace detail
 
 custom_authorities_plugin::custom_authorities_plugin() :
    my( new detail::custom_authorities_plugin_impl(*this) )
-{
-}
+{}
 
 custom_authorities_plugin::~custom_authorities_plugin()
-{
-}
+{}
 
 std::string custom_authorities_plugin::plugin_name()const
 {
    return "custom_authorities_plugin";
 }
+   
 std::string custom_authorities_plugin::plugin_description()const
 {
    return "custom_authorities_plugin description";
@@ -86,22 +68,10 @@ void custom_authorities_plugin::plugin_set_program_options(
    boost::program_options::options_description& cli,
    boost::program_options::options_description& cfg
    )
-{
-   cli.add_options()
-         ("custom_authorities_plugin_option", boost::program_options::value<std::string>(), "custom_authorities_plugin option")
-         ;
-   cfg.add(cli);
-}
+{}
 
 void custom_authorities_plugin::plugin_initialize(const boost::program_options::variables_map& options)
 {
-   database().applied_block.connect( [&]( const signed_block& b) {
-      my->onBlock(b);
-   } );
-
-   if (options.count("custom_authorities_plugin")) {
-      my->_plugin_option = options["custom_authorities_plugin"].as<std::string>();
-   }
 }
 
 void custom_authorities_plugin::plugin_startup()
